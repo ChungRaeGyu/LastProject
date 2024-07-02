3ba1b7e007853b89985282922ac9cd701ba591ce
00janjan00 <dnlaudtjs98@gmail.com>
2024-07-02 18:15:42 +0900

    add anim
    애니메이션 방식만추가

--- Assets/MS/Monsters/Animation/AttackAnim.cs
+++ Assets/MS/Monsters/Animation/AttackAnim.cs
@@ -2,14 +2,27 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AttackAnim : MonoBehaviour
 {
-     public void MonsterAttackAnim()
-     {
-        transform.DOMoveX(660, 1).OnComplete(() =>
+    public Button moveButton;
+
+    private void Start()
+    {
+        moveButton.onClick.AddListener(MonsterAttackAnim2);
+    }
+
+    public void MonsterAttackAnim(float startPosition, float endPosition, float duration) // 공격 모션
+    {
+        transform.DOLocalMoveX(startPosition, duration).OnComplete(() =>
         {
-            transform.DOMoveX(700, 1);
+            transform.DOLocalMoveX(endPosition, duration);
         });
-     }
+    }
+
+    public void MonsterAttackAnim2() // 흔들리는 모션
+    {
+        transform.DOShakePosition(0.5f, new Vector3(10f, 0, 0), 15, 13, false, false);
+    }
 }
