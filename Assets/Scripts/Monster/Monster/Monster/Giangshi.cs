dc18c79cefd9dad2d5906d2237ac870e667ed9c3
00janjan00 <dnlaudtjs98@gmail.com>
2024-08-06 21:29:10 +0900

    add monster, prefabs
    불골렘, 강시 추가, 프리팹 추가

---
+++ Assets/Scripts/Monster/Monster/Monster/Giangshi.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Giangshi : MonsterCharacter
+{
+    public HpBar healthBarPrefab;
+    private HpBar healthBarInstance;
+
+    private int attackRandomValue;
+
+    private new void Start()
+    {
+        base.Start();
+
+        Canvas canvas = UIManager.instance.healthBarCanvas;
+        if (canvas != null && healthBarPrefab != null)
+        {
+            // healthBarPrefab을 canvas의 자식으로 생성
+            healthBarInstance = Instantiate(healthBarPrefab, canvas.transform);
+            healthBarInstance.Initialized(currenthealth, currenthealth, hpBarPos);
+        }
+
+        attackRandomValue = random.Next(0, 10);
+
+        attackDescriptionText.text = $"<color=#FF7F50><size=30><b>공격</b></size></color>\n 이 적은 <color=#FFFF00>{attackRandomValue}</color>의 피해로 공격하려고 합니다.";
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        // 얼면 아무것도 띄우지 않는다.
+        if (isFrozen)
+        {
+            attackDescriptionText.text = "";
+        }
+    }
+
+    public override void TakeDamage(int damage)
+    {
+        base.TakeDamage(damage);
+
+        if (healthBarInstance != null)
+        {
+            healthBarInstance.ResetHealthSlider(currenthealth);
+            healthBarInstance.UpdatehealthText();
+        }
+    }
+
+    public void StartMonsterTurn()
+    {
+        StartCoroutine(MonsterTurn());
+    }
+
+    public override IEnumerator MonsterTurn()
+    {
+        if (GameManager.instance.player?.IsDead() == true) yield break;
+
+        // 부모 클래스의 MonsterTurn을 호출하여 얼리는 효과 적용
+        yield return base.MonsterTurn();
+
+        if (!isFrozen)
+        {
+            monsterNextAction.gameObject.SetActive(false);
+
+            // 행동 이미지에 연출을 줌
+
+            yield return new WaitForSeconds(1f); // 연출을 위한 대기
+
+            yield return PerformAttack(attackRandomValue);
+        }
+
+        yield return new WaitForSeconds(1f); // 연출을 위한 대기
+
+        attackRandomValue = random.Next(0, 10);
+
+        attackDescriptionText.text = $"<color=#FF7F50><size=30><b>공격</b></size></color>\n 이 적은 <color=#FFFF00>{attackRandomValue}</color>의 피해로 공격하려고 합니다.";
+
+        GameManager.instance.EndMonsterTurn();
+    }
+
+    protected override void Die()
+    {
+        GameManager.instance.RemoveMonsterDead(this);
+
+        base.Die();
+    }
+}
