60c1ed466537320e8894aea7a64707ff5cc639a1
fusil5577 <qwet676@gmail.com>
2024-08-16 18:20:58 +0900

    힘 듀오 카드 수정

--- Assets/Scripts/Card/CardScripts/Wizard/LatentPotential.cs
+++ Assets/Scripts/Card/CardScripts/Wizard/LatentPotential.cs
@@ -19,7 +19,7 @@ public class LatentPotential : CardBasic
     private void Update()
     {
         if (SceneManager.GetActiveScene().buildIndex == 3)
-            descriptionText.text = $"사용되지 않은 카드 수 <color=#00FF00><b>{DataManager.Instance.deck.Count}</b></color> X 3 만큼 피해를 줍니다.";
+            descriptionText.text = $"뽑을 카드 수 <color=#00FF00><b>{DataManager.Instance.deck.Count}</b></color> X 3 만큼 피해를 줍니다.";
     }
 
     public override void SetDescription()
@@ -41,7 +41,7 @@ public class LatentPotential : CardBasic
                 cardCountText = "X"; // 카드 수를 대신하는 X
             }
 
-            descriptionText.text = $"사용되지 않은 카드 수 <color={color}><b>{cardCountText}</b></color> X 3 만큼 피해를 줍니다.";
+            descriptionText.text = $"뽑을 카드 수 <color={color}><b>{cardCountText}</b></color> X 3 만큼 피해를 줍니다.";
         }
     }
 
