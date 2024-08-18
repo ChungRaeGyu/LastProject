86382ad703b5edeaef92e12d5bf9f246cb53d3f8
RaeGyu <wogns9769@gmail.com>
2024-08-18 21:44:05 +0900

    Add
    희귀 카드 한장
    일반 카드 한장
    추가했습니다.

---
+++ Assets/Scripts/Card/CardScripts/Warrior/FatalAttack.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FatalAttack : CardBasic
+{
+    private BezierDragLine bezierDragLine;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        bezierDragLine = GetComponent<BezierDragLine>();
+
+        SetDescription();
+    }
+
+    public override void SetDescription()
+    {
+        base.SetDescription();
+
+        if (descriptionText != null)
+        {
+            string color;
+
+            // 초기 ability와 현재 ability 비교
+            if (damageAbility > initialDamageAbility)
+            {
+                color = "#00FF00"; // 초록색
+            }
+            else if (damageAbility < initialDamageAbility)
+            {
+                color = "#FF0000"; // 빨간색
+            }
+            else
+            {
+                color = ""; // 기본 색
+            }
+
+            descriptionText.text = color == ""
+                ? $"<b>{damageAbility}</b> 만큼 피해를 줍니다.2턴동안 취약과 약화를 줍니다. "
+                : $"<color={color}><b>{damageAbility}</b></color> 만큼 피해를 줍니다.2턴동안 취약과 약화를 줍니다.";
+        }
+    }
+
+    public override IEnumerator TryUseCard()
+    {
+        MonsterCharacter targetMonster = bezierDragLine.detectedMonster;
+        if (targetMonster != null && GameManager.instance.player != null)
+        {
+            bezierDragLine.DestroyAimingImage();
+
+            GameManager.instance.player.UseCost(cost);
+
+            if (GameManager.instance.volumeUp > 0)
+            {
+                GameManager.instance.volumeUp -= 1;
+                CardUse(targetMonster);
+
+                yield return new WaitForSeconds(1f);
+            }
+
+            CardUse(targetMonster);
+
+            DataManager.Instance.AddUsedCard(cardBasic);
+
+            GameManager.instance.handManager.RemoveCard(transform);
+            Destroy(gameObject);// 카드를 사용했으므로 카드를 제거
+
+            GameManager.instance.CheckAllMonstersDead();
+        }
+    }
+
+    public void CardUse(MonsterCharacter targetMonster)
+    {
+        GameManager.instance.effectManager.PhysicalAttack(this, targetMonster);
+        targetMonster.WeakForTurns(utilAbility, 0.25f);
+        targetMonster.DefDownForTurns(utilAbility,0.5f);
+
+        SettingManager.Instance.PlaySound(CardClip1);
+        PlayPlayerAttackAnimation();
+    }
+
+    #region 특수카드 사용
+
+    #endregion
+
+    private void PlayPlayerAttackAnimation()
+    {
+        if (GameManager.instance.player != null && GameManager.instance.player.animator != null)
+        {
+            GameManager.instance.player.animator.SetTrigger("Attack");
+        }
+    }
+
+    // 강화 단계에 따른 능력치 적용
+    public override void ApplyEnhancements()
+    {
+        base.ApplyEnhancements();
+
+        switch (enhancementLevel)
+        {
+            case 1:
+                damageAbility += 2; // 데미지 증가
+                break;
+            case 2:
+                damageAbility += 4; // 데미지 증가
+                break;
+            default:
+                break;
+        }
+
+        SetDescription();
+    }
+}
